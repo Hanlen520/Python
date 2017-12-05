119f1697f9cf0645f6916b3b23e5b95609126da9
Crisimple <1240281595@qq.com>
2017-12-06 07:59:58 +0800

    git first commit & git init

---
+++ pythonBase.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# python基础
+# 一、数据类型和变量
+print("字符串：",'I\'m \"OK\"!');
+print('多行输出:\n','''...line1
+...line2
+...line3
+''');
+print("原样式输出：", r'Hello, "Bart"');
+
+print("布尔值--- 3 > 2 :", 3 > 2);
+print("布尔值--- True and True :", True and True);
+print("布尔值--- True and False :", True and False);
+print("布尔值--- 5 > 3 or 1 > 3 :", 5 > 3 or 1 > 3);
+print("布尔值--- 5 > 3 and not True :", 5 > 3 and not True);
+
+print("常量---浮点除/：", 9 / 3);
+print("常量---板除//：", 10 // 3);
+print("常量---取余%：", 10 % 3);
---
+++ sayHello.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+name = input("Hi, please enter your name: ");
+print("Hello ", name);
+print("=====END======")
+
+# if...else...
+variable = 100
+if variable >= 0:
+    print(variable, 'great than zero');
+else:
+    print(variable, 'less than zero');
+print("=====END======")
+
+
+
